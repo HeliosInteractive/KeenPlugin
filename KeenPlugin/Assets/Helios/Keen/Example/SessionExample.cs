c964277ed2bd8118014de220daa7cfa572136f3b
Sepehr Laal <sepehr.laal@gmail.com>
2016-03-23 12:43:18 -0700

    checked in an example for SessionAwareClient

---
+++ KeenPlugin/Assets/Helios/Keen/Example/SessionExample.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(Helios.Keen.Client))]
+public class SessionExample : MonoBehaviour
+{
+    public Helios.Keen.SessionAwareClient MetricsClient;
+
+    void Awake()
+    {
+        // Always make sure you have a Client instance attached.
+
+        if (MetricsClient == null)
+            MetricsClient = GetComponent<Helios.Keen.SessionAwareClient>();
+
+        if (MetricsClient == null)
+            Debug.LogError("Did you forget to set the reference to Helios.KeenClient?");
+    }
+
+    void Start()
+    {
+        // This line assigns project settings AND starts
+        // client instance's cache service if everything is OK.
+
+        MetricsClient.Settings = new Helios.Keen.Client.Config
+        {
+            /* [REQUIRED] Keen.IO project id, Get this from Keen dashboard */
+            ProjectId           = "none",
+            /* [REQUIRED] Keen.IO write key, Get this from Keen dashboard */
+            WriteKey = "none",
+            /* [OPTIONAL] Attempt to sweep the cache every 45 seconds */
+            CacheSweepInterval  = 45.0f,
+            /* [OPTIONAL] In every sweep attempt pop 10 cache entries */
+            CacheSweepCount     = 10,
+            /* [OPTIONAL] This is the callback per Client's event emission */
+            EventCallback       = OnKeenClientEvent,
+            /* [OPTIONAL] A cache implementation instance. If not provided, cache is turned off */
+            CacheInstance       = new Helios.Keen.Cache.Sqlite(Path.Combine(Application.streamingAssetsPath, "keen.sqlite3"))
+        };
+    }
+
+    void OnKeenClientEvent(Helios.Keen.Client.CallbackData metric_event)
+    {
+        Debug.LogFormat("Keen event with name {0} and value {1} ended with status: {2}",
+            metric_event.name, metric_event.data, metric_event.status.ToString());
+    }
+
+    void Update ()
+    {
+        // Let's send a bunch of metric events on mouse click...
+
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        // Start a new session with a random UUID
+        MetricsClient.StartSession();
+
+        // This is an example of sending Helios specific events
+        MetricsClient.SendQuizEvent(new Helios.Keen.Client.QuizEvent
+        {
+            quizId = "IQ test",
+            quizResult = "failed",
+            experienceData  = new Helios.Keen.Client.ExperienceData
+            {
+                experienceLabel = "Keen Plugin",
+                versionNumber   = "1.0.0",
+                location        = "never land"
+            }
+        });
+
+        // End current session
+        MetricsClient.EndSession(Helios.Keen.Client.EndSessionType.Completed);
+
+        // Start a new session with a known guestId
+        MetricsClient.StartSession(new Helios.Keen.Client.Session { guestId = "myGuestId" });
+
+        // This is an example of using custom data types
+        MetricsClient.SendEvent("custom_event", new CustomData
+        {
+            data_member_1 = "test string",
+            data_member_2 = 25000.0f,
+            data_member_3 = new CustomNestedData
+            {
+                data_member_1 = "\"nested\" string",
+                data_member_2 = 25000d,
+            }
+        });
+
+        // End current session
+        MetricsClient.EndSession(Helios.Keen.Client.EndSessionType.Abandoned);
+    }
+
+    class CustomData
+    {
+        public string           data_member_1;
+        public float            data_member_2;
+        public CustomNestedData data_member_3;
+    }
+
+    class CustomNestedData
+    {
+        public string   data_member_1;
+        public double   data_member_2;
+    }
+}
